981b4d19e95e22ce82ef23a7006178e0f1b5aca3
dtrans2022 <datatransform.info@gmail.com>
2026-08-20 13:58:08 +1000

    Auto-redirect base URL to latest web bundle

--- mobile/scripts/prepare-gh-pages.cjs
+++ mobile/scripts/prepare-gh-pages.cjs
@@ -10,7 +10,17 @@ if (!fs.existsSync(indexPath)) {
 }
 
 const html = fs.readFileSync(indexPath, "utf8");
-const normalized = html.replace(/src="\/_expo\//g, 'src="./_expo/');
+const bundleMatch = html.match(/src="\/_expo\/static\/js\/web\/(index-[^"]+\.js)"/);
+
+if (!bundleMatch) {
+  throw new Error("Unable to determine exported web bundle hash from index.html");
+}
+
+const bundleName = bundleMatch[1];
+const redirectScript = `<script>(function(){try{var url=new URL(window.location.href);if(!url.searchParams.get('v')){url.searchParams.set('v','${bundleName.replace(/^index-/, "").replace(/\.js$/, "")}');window.location.replace(url.toString());}}catch(e){}})();</script>`;
+const normalized = html
+  .replace(/src="\/_expo\//g, 'src="./_expo/')
+  .replace("<head>", `<head>${redirectScript}`);
 
 fs.writeFileSync(indexPath, normalized, "utf8");
 fs.writeFileSync(noJekyllPath, "", "utf8");
